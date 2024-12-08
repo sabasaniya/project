7861403d9dfbc34a4f22a5de5eb31529b70d45df
saba <sabasaniya90592@gmail.com>
2024-12-08 23:04:31 +0530

    Sample.java

---
+++ Sample.java
@@ -0,0 +1,15 @@
+// Java Program to convert char Array to String:
+public class Sample {
+
+ public static void main(String[] args) {
+
+ char[] charArray = {'h', 'e', 'l', 'l', 'o'};
+
+ String str = new String(charArray);
+
+ System.out.println("Converted String: " + str);
+
+ }
+
+}
+
